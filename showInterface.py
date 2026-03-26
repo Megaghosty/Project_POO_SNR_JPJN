daab7c092ca27216f22d47a2b58c72103aa69377
Johan Pavis <johan.pavis@etudiant.univ-rennes.fr>
2026-03-26 18:15:17 +0100

    debut affichage

--- showInterface.py
+++ showInterface.py
@@ -1,18 +1,21 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMainWindow
+from Interface import Ui_MainWindow
 
 app = QApplication(sys.argv)
 
 class MainWindow(QMainWindow) :
     def __init__(self) :
         super().__init__()
-        # Initialisation d'un objet avec la classe créée avec QT Designer
-        self.ui = Ui_Form()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.btn_equipes.clicked.connect(self.widget_equipe)
+
+    def widget_equipe(self):
+        self.stackedWidget.setCurrentWidget(self.widget_home)
 
 window = MainWindow()
 window.setWindowTitle("Ma première interface PyQt6")
-window.setGeometry(200, 200, 400, 200)
 window.show()
 app.exec()
 sys.exit()
