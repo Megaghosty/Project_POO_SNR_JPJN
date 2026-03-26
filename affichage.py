4216cb2de5ae637cac0446732873e3dd57a5fe15
Johan Pavis <johan.pavis@etudiant.univ-rennes.fr>
2026-03-26 16:32:11 +0100

    ajout fichier UML

--- affichage.py
+++ affichage.py
@@ -1,42 +1,16 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMainWindow
 
 app = QApplication(sys.argv)
 
-class FenetrePrincipale(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
-        # Initialisation d'un label de resultat
-        self.labelResultat = QLabel("...",self)
-        self.labelResultat.move(100,115)
-        # Initialisation d'un label du nombre 1
-        self.labelNombre1 = QLabel("Nombre 1",self)
-        self.labelNombre1.move(30,10)
-        # Initialisation d'un label du nombre 2
-        self.labelNombre2 = QLabel("Nombre 2",self)
-        self.labelNombre2.move(30,40)
-        # Initialisation d'un lineEdit du nombre 1
-        self.lineEditNombre1 = QLineEdit("",self)
-        self.lineEditNombre1.move(100,10)
-        # Initialisation d'un lineEdit du nombre 2
-        self.lineEditNombre2 = QLineEdit("",self)
-        self.lineEditNombre2.move(100,40)
-        # Initialisation d'un boutton pour le calcule de la somme
-        self.bouttonValidation = QPushButton("Calculer la somme",self)
-        # Relier l'appuie du boutton à la méthode calculer()
-        self.bouttonValidation.clicked.connect(self.calculer)
-        self.bouttonValidation.move(80,75)
+class MainWindow(QMainWindow) :
+    def __init__(self) :
+        super().__init__()
+        # Initialisation d'un objet avec la classe créée avec QT Designer
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
 
-    def calculer(self):
-        # Récupération des valeurs
-        n1 = int(self.lineEditNombre1.text())
-        n2 = int(self.lineEditNombre2.text())
-        # Modification du texte de resultat
-        self.labelResultat.setText(f"La somme de {n1} + {n2} est {n1+n2}")
-        # Ajustement de la taille du label au texte
-        self.labelResultat.adjustSize()
-
-window = FenetrePrincipale()
+window = MainWindow()
 window.setWindowTitle("Ma première interface PyQt6")
 window.setGeometry(200, 200, 400, 200)
 window.show()
